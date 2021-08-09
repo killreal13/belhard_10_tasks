1cb117db16da3d092408034d112a5e1ac219604d
yungun <kirill.dyuzhij@gmail.com>
2021-08-09 22:42:47 +0300

    new solves
    *all solved

--- tasks/csv_tasks/population.py
+++ tasks/csv_tasks/population.py
@@ -3,3 +3,16 @@
 файла world_population.csv и функцию должна возвращать год, в котором
 наибольший процент прироста населения.
 """
+import csv
+
+
+def max_delta(some_file: csv) -> int:
+    with open(some_file, 'r') as csv_file:
+        writer = csv.DictReader(csv_file)
+        result_dict = dict()
+        for value in writer:
+            result_dict[value['Year']] = value['ChangePerc']
+        max_value = max(result_dict.values())
+        for year, perc in result_dict.items():
+            if perc == max_value:
+                return year
--- tasks/file_tasks/reverse_rows.py
+++ tasks/file_tasks/reverse_rows.py
@@ -3,3 +3,14 @@
 файл. Создать новый файл, каждая строка которого получается из соответствующей
 строки исходного файла перестановкой слов в обратном порядке.
 """
+
+
+def revert_rows(file_path: str):
+    with open(file_path, "r") as file:
+        reverted_file = open("reverted_file.txt", "w")
+        for line in file.readlines():
+            new_line = list()
+            new_line.append(line)
+            new_line = new_line[0].split(" ")[::-1]
+            new_line = ' '.join(new_line).replace("\n", "").replace(".", "") + "."
+            reverted_file.write(f"{new_line}\n")
